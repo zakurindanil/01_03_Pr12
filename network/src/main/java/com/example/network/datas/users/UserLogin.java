9fe90c1edcb0e3e6de658f5ff93f7e8db7024c81
danil <danil@DESKTOP-T4KQBOA>
2026-06-09 17:03:31 +0500

    разработанный класс авторизации закурин

---
+++ network/src/main/java/com/example/network/datas/users/UserLogin.java
@@ -0,0 +1,39 @@
+package com.example.network.datas.users;
+
+import com.example.network.domains.apis.MyAsyncTask;
+import com.example.network.domains.callbacks.MyResponseCallback;
+import com.example.network.domains.common.Settings;
+import com.example.network.domains.models.User;
+import com.google.gson.GsonBuilder;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class UserLogin extends MyAsyncTask {
+    User user;
+    public UserLogin(User user, MyResponseCallback callback){
+        super(callback);
+        this.user = user;
+    }
+
+    @Override
+    protected String doInBackground (Void... voids){
+        String rawData = new GsonBuilder().create().toJson(user);
+        try{
+            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/login")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.POST)
+                    .header("Content-type", "application/json")
+                    .requestBody(rawData)
+                    .execute();
+            return response.statusCode() == 200 ?
+                    response.body() :
+                    "Error:" + response.body();
+        } catch (IOException e){
+            return "Error: " + e.getMessage();
+        }
+    }
+}
