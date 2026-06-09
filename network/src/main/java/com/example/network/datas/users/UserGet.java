534970ac81fea69b75d237f9519565886e4ee77a
danil <danil@DESKTOP-T4KQBOA>
2026-06-09 22:53:31 +0500

    разработанный класс получения информации о пользователе закурин

--- network/src/main/java/com/example/network/datas/users/UserGet.java
+++ network/src/main/java/com/example/network/datas/users/UserGet.java
@@ -3,27 +3,26 @@ package com.example.network.datas.users;
 import com.example.network.domains.apis.MyAsyncTask;
 import com.example.network.domains.callbacks.MyResponseCallback;
 import com.example.network.domains.common.Settings;
+import com.example.network.domains.models.User;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
-import java.util.SplittableRandom;
 
-public class UserLogout extends MyAsyncTask {
+public class UserGet extends MyAsyncTask {
     String token;
-
-    public UserLogout(String token, MyResponseCallback callback){
+    public UserGet(String token, MyResponseCallback callback){
         super(callback);
         this.token = token;
     }
     @Override
     protected String doInBackground(Void... voids){
         try{
-            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/logout")
+            Connection.Response response = Jsoup.connect(Settings.URL + "api/user/get")
                     .ignoreContentType(true)
                     .ignoreHttpErrors(true)
-                    .method(Connection.Method.POST)
+                    .method(Connection.Method.GET)
                     .header("Content-type", "application/json")
                     .header("token", token)
                     .execute();
